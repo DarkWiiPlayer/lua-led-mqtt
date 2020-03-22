7a4010e849a0cc490ca737071ad251b699c5255d
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2020-03-22 13:31:30 +0100

    Implement DarkLED protocol

---
+++ darkled.lua
@@ -0,0 +1,71 @@
+local ws2812 = require 'ws2812'
+ws2812.init()
+
+local config = require 'config'
+local Timer = require 'timer'
+
+local timer = Timer.new()
+local buffer = ws2812.newBuffer(config.ledcount, 3)
+
+local function switch_rg(tab) tab[1], tab[2] = tab[2], tab[1]; return tab end
+
+ws2812.write(buffer)
+
+local function interpolate(from, to, value, max)
+	if max then value = value/max end
+	return
+		from[1] + (to[1]-from[1])*value,
+		from[2] + (to[2]-from[2])*value,
+		from[3] + (to[3]-from[3])*value
+end
+
+local commands = {}
+
+function commands.fill(data)
+	timer:stop()
+	buffer:fill(unpack(switch_rg(data.color)))
+	ws2812.write(buffer)
+end
+
+function commands.fade(data)
+	local t = 50e-3
+	timer:set(t*1e3, function()
+		local time = data.time * 1/t
+		local from = switch_rg(data.from)
+		local to = switch_rg(data.to)
+
+		for i=1,time do
+			buffer:fill(interpolate(from, to, i, time))
+			ws2812.write(buffer)
+			coroutine.yield()
+		end
+	end)
+end;
+
+function commands.wave(data)
+	local wait_next_frame = coroutine.yield
+	local t = 50e-3
+	local low = switch_rg(data.low)
+	local high = switch_rg(data.high)
+	local tmin, tdmax = data.min, data.max
+	timer:set(t*1e3, function()
+		while true do
+			local time = (tmin+math.random()*tdmax) / t
+			for i=1,time do
+				buffer:fill(interpolate(high, low, i, time))
+				ws2812.write(buffer)
+				wait_next_frame()
+			end
+			high, low = low, high
+		end
+	end)
+end
+
+return function(data)
+	if config.path and data.path and config.path:find(data.path)~=1 then
+		return true, "Wrong device path, skipping"
+	end
+	if commands[data.command] then
+		return commands[data.command](data)
+	end
+end
