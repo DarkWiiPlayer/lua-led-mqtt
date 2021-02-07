80efa84934be84e4e2c078170abb91b318fb76c0
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2021-02-07 18:12:23 +0100

    Add MQTT connected message

--- listener.lua
+++ listener.lua
@@ -9,6 +9,7 @@ return function(num_leds)
 	print("Connecting to MQTT broker on "..config.broker.ip.."...")
 	client:connect(config.broker.ip, config.broker.port, false, function(client)
 		print("Connected to MQTT broker on "..config.broker.ip.."!")
+		client:publish('nodemcu/led/status', json.encode{name=config.name, status="online", message=config.messages.status.online}, 0, 0)
 
 		for _, topic in ipairs(config.topics) do
 			client:subscribe(topic, 0)
