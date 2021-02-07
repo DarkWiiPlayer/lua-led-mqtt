f75ba42746e82c8c69d3ad89c41816f46018e35a
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2021-02-07 15:37:53 +0100

    Added debug mode when pin 0 is grounded
    Grounding pin 0 will now cause init.lua to skip the main program to
    allow debugging or uploading files instead.

--- init.lua
+++ init.lua
@@ -1,4 +1,10 @@
-print(pcall(function()
-	local config = require 'config'
-	require 'enable-wifi' (config.name, require 'listener')
-end))
+gpio.mode(0, gpio.INPUT, gpio.PULLUP)
+
+if gpio.read(0) == gpio.HIGH then
+	print(pcall(function()
+		local config = require 'config'
+		require 'enable-wifi' (config.name, require 'listener')
+	end))
+else
+	print("Pin 0 is grounded: entering debug mode")
+end
